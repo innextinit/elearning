eb7dd99ebab2c8ad467fb15d46bbceecbe7c3d3f
innext <inni22121922@gmail.com>
2020-07-03 01:17:25 +0100

    fixed the styling isues not seen by other part of the code

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
-var Message = require('../models/message');
+const Message = require('../models/message');
+const Class = require('../models/class');
 
 router.get('/contact', ensureAuthenticated, function(req, res) {
     res.render('contact', {layout: false});
 });
 
 router.post('/contact', ensureAuthenticated, function(req, res) {
-    var firstname = req.params.firstname;
-    var lastname = req.params.lastname;
-    var email = req.params.email;
-    var subject = req.params.subject;
-    var message = req.params.message;
-    var username = req.params.username;
+    const firstname = req.params.firstname;
+    const lastname = req.params.lastname;
+    const email = req.params.email;
+    const subject = req.params.subject;
+    const message = req.params.message;
+    const username = req.params.username;
 
 // form validation
   check('firstname', 'character allowed is a-z,A-Z')
@@ -69,7 +70,7 @@ router.post('/contact', ensureAuthenticated, function(req, res) {
         message: message
       });
     } else {
-        var newMessage = new Message({
+        const newMessage = new Message({
             firstname: firstname,
             lastname: lastname,
             email: email,
@@ -85,46 +86,116 @@ router.post('/contact', ensureAuthenticated, function(req, res) {
 
     req.flash()
 
-})
+});
 
 router.get('/business', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/business', {layout: false});
+    }
+  });
 });
 
 router.get('/health-psychology', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/health-psychology', {layout: false});
+    }
+  });
 });
 
 router.get('/accounting', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/accounting', {layout: false});
+    }
+  });
 });
 
 router.get('/it-software', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/it-software', {layout: false});
+    }
+  });
 });
 
 router.get('/art-media', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/art-media', {layout: false});
+    }
+  });
 });
 
 router.get('/office-productivity', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/office-productivity', {layout: false});
+    }
+  });
 });
 
 router.get('/language-lifestyle', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/language-lifestyle', {layout: false});
+    }
+  });
 });
 
 router.get('/web-programming', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/web-programming', {layout: false});
+    }
+  });
 });
 
 router.get('/design', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/design', {layout: false});
+    }
+  });
 });
 
 router.get('/music', function(req, res) {
+  Class.getClasses(function(err, foundclasses){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
   res.render('categories/music', {layout: false});
+    }
+  });
 });
 
 function ensureAuthenticated(req, res, next){
